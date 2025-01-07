25ad60a5da11553d8ac0ac475d3f4d250d73c18b
KMS322 <kms930322@naver.com>
2025-01-07 15:22:04 +0900

    250107_04

--- src/components/main/contact.js
+++ src/components/main/contact.js
@@ -3,7 +3,26 @@ import "../../css/contact.css";
 const Contact = () => {
   return (
     <div className="contact">
-      <p>contact</p>
+      <div className="text_box">
+        <p>
+          Pets communicate with their tails, and pet owners connect through
+          TalkTail.
+        </p>
+      </div>
+      <div className="article_container">
+        <div className="article1">
+          <p>Contact us</p>
+          <p>Thank you for visiting</p>
+          <hr />
+          <p>Tel : 82)10-4026-5955</p>
+          <p>e-mail : creamoff2021@creamoff.co.kr</p>
+        </div>
+        <div className="article2">
+          <p>Business Hours</p>
+          <p>Weekdays : am.09:00~pm.18:00</p>
+          <p>Closed on weekends</p>
+        </div>
+      </div>
     </div>
   );
 };
